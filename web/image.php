2a3ca7f01d3fd1eaa77081c89a622b14795787f9
ismailbayar <31563160+ismailbayar@users.noreply.github.com>
2017-09-02 17:31:16 +0300

    Update image.php

--- web/image.php
+++ web/image.php
@@ -1,6 +1,5 @@
 <?php
-header("Content-Type:image/jpeg");
 $id = $_GET["id"];
-$link = "http://185.181.210.253/uploads/thumbs/'".$id."'-social.jpg";
-echo file_get_contents($link);
+$link = "http://185.181.210.253/uploads/thumbs/".$id."-social.jpg";
+echo $link;
 ?>
